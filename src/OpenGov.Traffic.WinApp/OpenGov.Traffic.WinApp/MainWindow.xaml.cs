b7324df355ac4f49dbc73c9b013758f3c9f21fa8
J.T <gisfromscratch@live.de>
2019-03-24 22:49:05 +0100

    #1 Mahapps hinzufügen und MetroWindow verwenden

--- src/OpenGov.Traffic.WinApp/OpenGov.Traffic.WinApp/MainWindow.xaml.cs
+++ src/OpenGov.Traffic.WinApp/OpenGov.Traffic.WinApp/MainWindow.xaml.cs
@@ -19,7 +19,7 @@ namespace OpenGov.Traffic.WinApp
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow
     {
         public MainWindow()
         {
