8885d95b260f6f213da2c26679a73bc7b5e8c2d8
J.T <gisfromscratch@live.de>
2019-03-31 16:38:52 +0200

    #3 .NET Standard und .NET Full Framework referenzieren und nicht mehr die .NET Core Bibliotheken

--- src/OpenGov.Traffic.WinApp/OpenGov.Traffic.Services/TrafficService.cs
+++ src/OpenGov.Traffic.WinApp/OpenGov.Traffic.Services/TrafficService.cs
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
+ 
 using Anywhere.ArcGIS;
 using Anywhere.ArcGIS.GeoJson;
 using Newtonsoft.Json;
--- src/OpenGov.Traffic.WinApp/OpenGov.Traffic.WinApp/MapViewModel.cs
+++ src/OpenGov.Traffic.WinApp/OpenGov.Traffic.WinApp/MapViewModel.cs
@@ -1,35 +1,44 @@
+/*
+ * Copyright 2019 Jan Tschada
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 using System;
-using System.Collections.Generic;
 using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Runtime.CompilerServices;
-using Esri.ArcGISRuntime.Data;
-using Esri.ArcGISRuntime.Geometry;
-using Esri.ArcGISRuntime.Location;
 using Esri.ArcGISRuntime.Mapping;
-using Esri.ArcGISRuntime.Security;
-using Esri.ArcGISRuntime.Symbology;
-using Esri.ArcGISRuntime.Tasks;
-using Esri.ArcGISRuntime.UI;
 
 namespace OpenGov.Traffic.WinApp
 {
     /// <summary>
-    /// Provides map data to an application
+    /// Verwaltet die Kartendaten für diese Anwendung.
     /// </summary>
     public class MapViewModel : INotifyPropertyChanged
     {
-        public MapViewModel()
-        {
-
-        }
-
-        private Map _map = new Map(Basemap.CreateStreets());
+        private Map _map;
 
         /// <summary>
-        /// Gets or sets the map
+        /// Erzeugt eine neue Instanz und setzt die Hintergrundkarte.
+        /// </summary>
+        public MapViewModel()
+        {
+            _map = new Map(Basemap.CreateStreets());
+            _map.Loaded += MapLoaded;
+        }
+
+        /// <summary>
+        /// Die Karte mit allen Ebenen dieser Anwendung.
         /// </summary>
         public Map Map
         {
@@ -37,10 +46,15 @@ namespace OpenGov.Traffic.WinApp
             set { _map = value; OnPropertyChanged(); }
         }
 
+        private void MapLoaded(object sender, EventArgs evt)
+        {
+            
+        }
+
         /// <summary>
-        /// Raises the <see cref="MapViewModel.PropertyChanged" /> event
+        /// Erzeugt das <see cref="MapViewModel.PropertyChanged" /> Ereignis.
         /// </summary>
-        /// <param name="propertyName">The name of the property that has changed</param>
+        /// <param name="propertyName">Name der Eigenschaft, welche verändert wurde</param>
         protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
         {
             var propertyChangedHandler = PropertyChanged;
